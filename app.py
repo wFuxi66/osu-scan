bdf221b6598bf8bcdca2b54799a1acada8f5b22e
Fuxi66 <ritorupenda@gmail.com>
2026-02-28 23:19:35 +0100

    update app port binding for render

--- app.py
+++ app.py
@@ -301,6 +301,7 @@ def trigger_global_scan():
     return jsonify({'status': 'started', 'incremental': incremental})
 
 if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
     print("Starting osu!scan...")
-    print("Open http://127.0.0.1:5000 in your browser")
-    app.run(debug=True, port=5000, threaded=True)
+    print(f"Server is binding to 0.0.0.0:{port}")
+    app.run(host='0.0.0.0', port=port, debug=False, threaded=True)
